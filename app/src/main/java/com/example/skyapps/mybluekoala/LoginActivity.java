1d549be3b7c66ec589279cb4e74f8a6a1122af80
Trashkat <onemereday@gmail.com>
2018-11-22 17:45:23 -0500

    Initial commit

---
+++ app/src/main/java/com/example/skyapps/mybluekoala/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.skyapps.mybluekoala;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+    Button IdEmpezar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        IdEmpezar = (Button) findViewById(R.id.IdEmpezar);
+
+        IdEmpezar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                Intent i = new Intent(LoginActivity.this, DispositivosBT.class);//<-<- PARTE A MODIFICAR >->-
+                startActivity(i);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/skyapps/mybluekoala/VideoActivity.java
@@ -0,0 +1,66 @@
+package com.example.skyapps.mybluekoala;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import java.net.URI;
+
+
+public class VideoActivity extends AppCompatActivity {
+    Uri uri;
+
+    {
+        uri = null;
+    }
+
+    VideoView videoView;
+    String DaVideo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+
+        videoView=(VideoView)findViewById(R.id.videoView);
+        DaVideo= getIntent().getStringExtra("url");
+
+
+
+
+        if(DaVideo.equals("A")){
+
+            uri = Uri.parse("http://innovaskyapps.com.co/UB/Vocales.mp4");
+
+
+
+
+
+        }
+
+       else if(DaVideo.equals("B")){
+
+            uri = Uri.parse("http://innovaskyapps.com.co/UB/Vocales.mp4");
+
+
+        }
+
+        else if(DaVideo.equals("C")){
+
+             uri = Uri.parse("http://innovaskyapps.com.co/UB/WhatsApp%20Video%202018-11-07%20at%2000.59.52.mp4");
+
+
+        }
+       /* else{
+
+             uri = Uri.parse("http://innovaskyapps.com.co/UB/WhatsApp%20Video%202018-11-07%20at%2000.59.52.mp4");
+        }*/
+
+        videoView.setMediaController(new MediaController(this));
+        videoView.setVideoURI(uri);
+        videoView.requestFocus();
+        videoView.start();
+    }
+}
